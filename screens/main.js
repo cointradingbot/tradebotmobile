6c1f181b8c2896e75781fa925a82b9c055257d4b
Phi Huynh <trumhemcut@hotmail.com>
2018-02-24 13:18:15 +0700

    Adding state

--- screens/main.js
+++ screens/main.js
@@ -8,8 +8,28 @@ import {
     View
 } from 'react-native';
 import { Divider, Subtitle, Caption, Image, Screen, ScrollView, Heading, NavigationBar, Icon, Title, Button, Row } from '@shoutem/ui'
+import { WSAEPROVIDERFAILEDINIT } from 'constants';
 
 export class MainPage extends Component<{}> {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            coin: 'ADA',
+            sellAccount: {
+                platform: 'Binance',
+                sellPrice: 0.00001234
+            },
+            buyAccount: {
+                platform: 'Bittrex',
+                buyPrice: 0.00001233
+            },
+            bidask: 0.00000001,
+            profit: 0,
+            coinQty: 1000
+        }
+    }
+
     componentDidMount() {
         console.log(Config.WEBSOCKET_URL)
         const socket = io(Config.WEBSOCKET_URL, {transports: ['websocket']})
@@ -44,9 +64,9 @@ export class MainPage extends Component<{}> {
                     <Row>
                         <Image styleName="small-avatar top" source={require('../assets/icons/ada.png')} />
                         <View styleName="vertical">
-                            <Subtitle>BITTREX > BINANCE</Subtitle>
+                            <Subtitle>{`${this.state.buyAccount.platform} > ${this.state.sellAccount.platform}`}</Subtitle>
                             <View styleName="horizontal stretch space-between">
-                                <Text styleName="multiline">Bid-Ask: 0.00001234</Text>
+                                <Text styleName="multiline">{`Bid-Ask: ${this.state.bidask}`}</Text>
                                 <Caption>20 minutes ago</Caption>
                             </View>
                         </View>
